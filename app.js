3874c33eb67073fd6b79be63852f3a83caf828b9
Dylan Sapienza <dylsapienza12@gmail.com>
2020-10-26 21:10:25 -0400

    Setup Deployment

--- app.js
+++ app.js
@@ -147,5 +147,21 @@ app.get('/testAPI', function(req, res){
   res.send("The API is working properly");
 })
 
+if (process.env.NODE_ENV === "production") {
+  app.use(
+    express.static(path.join(__dirname, "/client/build"))
+  );
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(
+        __dirname,
+        "client",
+        "build",
+        "index.html"
+      )
+    );
+  });
+}
+
 console.log('Listening on 8888');
 app.listen(8888);
